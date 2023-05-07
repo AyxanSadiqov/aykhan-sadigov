8d5ba94d1525cc0210a104f7b184e56045cf1a03
EDUSISCO\musta <ayxan.sadiq.98@gmail.com>
2023-05-07 19:40:41 +0300

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,234 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="about">
+    <div class="skills">Skills</div>
+    <div class="container">
+      <div class="card" @mousemove="cardMouseMove">
+        <div v-for="(item, index) in itemsFunc" :key="index" class="title">
+          <div class="title-border"></div>
+          <div class="title-content">
+            <div class="name">{{ item.name }}</div>
+            <div class="bar">
+              <div class="bar-bg">
+                <div
+                  class="bar-front"
+                  :style="`width:${item.performance || 0}%;`"
+                ></div>
+              </div>
+              <div class="bar-text">{{ item.performance || 0 }} %</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          name: "HTML",
+          performance: 100,
+        },
+        {
+          name: "JavaScript",
+          performance: 20,
+        },
+        {
+          name: "CSS",
+          performance: 80,
+        },
+        {
+          name: "Scss/Sass",
+          performance: 90,
+        },
+        {
+          name: "VueJS",
+          performance: 35,
+        },
+        {
+          name: "NuxtJS",
+          performance: 8,
+        },
+        {
+          name: "Tailwind",
+          performance: 50,
+        },
+        {
+          name: "Bootstrap",
+          performance: 75,
+        },
+        {
+          name: "Ajax",
+          performance: 100,
+        },
+        {
+          name: "AngularJS",
+          performance: 100,
+        },
+        {
+          name: "ReactJS",
+          performance: 100,
+        },
+        {
+          name: "jQuery",
+          performance: 100,
+        },
+        {
+          name: "TypeScript",
+          performance: 100,
+        },
+      ],
+    };
+  },
+  computed: {
+    itemsFunc() {
+      let items = this.items;
+      let sortedItem = items.sort((a, b) =>
+        a.performance < b.performance ? 1 : -1
+      );
+      return sortedItem;
+    },
+  },
+  methods: {
+    cardMouseMove(e) {
+      for (const card of document.getElementsByClassName("title")) {
+        const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#about {
+  height: 100vh;
+  overflow-y: auto;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background: black;
+  .skills{
+    display: flex;
+    justify-content: start;
+    max-width: 916px;
+    width: 100%;
+    color: #41a093;
+    font-size: 2rem;
+    font-weight: 600;
+  }
+}
+.container {
+  display: flex;
+  justify-content: center;
+  background: black;
+  -webkit-user-select: none; /* Safari */
+  -ms-user-select: none; /* IE 10 and IE 11 */
+  user-select: none; /* Standard syntax */
+}
+
+.card {
+  padding-top: 1rem;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+  max-width: 916px;
+  position: relative;
+}
+
+.card:hover > .title > .title-border {
+  opacity: 1;
+}
+
+.title {
+  max-width: 32%;
+  width: 100%;
+  height: 150px;
+  max-height: 150px;
+  position: relative;
+  background: black;
+  cursor: default;
+  border-radius: 5px;
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+
+  &:hover::before {
+    opacity: 1;
+  }
+  &::before,
+  .title-border {
+    border-radius: inherit;
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    z-index: 2;
+  }
+  &::before {
+    background: radial-gradient(
+      800px circle at var(--mouse-x) var(--mouse-y),
+      rgba(255, 255, 255, 0.06),
+      transparent 40%
+    );
+    z-index: 3;
+  }
+  .title-border {
+    background: radial-gradient(
+      400px circle at var(--mouse-x) var(--mouse-y),
+      rgba(255, 255, 255, 0.3),
+      transparent 40%
+    );
+    z-index: 1;
+  }
+  .title-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: rgb(23, 23, 23);
+    border-radius: inherit;
+    margin: 2px;
+    height: calc(100% - 4px);
+    width: calc(100% - 4px);
+    position: relative;
+    z-index: 2;
+    color: rgb(179, 179, 179);
+    .name {
+      font-size: 2rem;
+      font-weight: 600;
+    }
+    .bar {
+      padding-top: 1rem;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 50%;
+      .bar-text {
+        padding-top: 0.2rem;
+        font-size: 0.6rem;
+        width: 100%;
+      }
+      .bar-bg {
+        width: 100%;
+        height: 4px;
+        background: white;
+        border-radius: 5px;
+        overflow: hidden;
+        .bar-front {
+          background: #41a093;
+          height: 100%;
+          transition: all 0.3s;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,129 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="container">
+      <div class="tiles">
+        <div
+          v-for="(menu, index) in menus"
+          :key="index"
+          class="title"
+          @click="
+            activeMenu == index + 1 ? (activeMenu = -1) : (activeMenu = index + 1)
+          "
+          :class="activeMenu == index + 1 ? 'active disable' : ''"
+        >
+          <div @click="openUrl(menu, index)" class="title-content">
+            <div class="num">0{{ index + 1 }}.</div>
+            <div class="text">{{ menu.name }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      activeMenu: -1,
+      menus: [
+        {
+          name: "About",
+          router: "about"
+        },
+        {
+          name: "Experience",
+          router: "experience"
+        },
+        {
+          name: "Work",
+          router: "work"
+        },
+        {
+          name: "Contact",
+          router: "contact"
+        },
+      ],
+    };
+  },
+  methods: {
+    openUrl(menu, index) {
+      if(this.activeMenu == index + 1) {
+        setTimeout(() => {
+          this.$router.push({ name: menu.router })
+        }, 1000)
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.active {
+  transform: rotateX(-90deg) rotateY(45deg) rotateZ(0deg) translate3d(0, -120px, 0);
+  background: #cad5f5 !important;
+  cursor: pointer !important;
+}
+.container {
+  -webkit-perspective: 500px;
+  perspective: 1000px;
+  height: 100vh;
+  -webkit-user-select: none; /* Safari */
+  -ms-user-select: none; /* IE 10 and IE 11 */
+  user-select: none; /* Standard syntax */
+}
+
+.tiles {
+  width: 630px;
+  height: 630px;
+
+  position: relative;
+  left: 50%;
+  top: 0px;
+  margin-left: -315px;
+
+  -webkit-transform: rotateZ(0deg);
+  transform: rotateX(60deg) rotateY(0deg) rotateZ(45deg);
+  -webkit-transform-style: preserve-3d;
+  transform-style: preserve-3d;
+  // border: 1px solid red;
+}
+
+.title {
+  width: 300px;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  float: left;
+  margin: 0 10px 10px 0;
+  background: #858dad;
+  color: white;
+  cursor: default;
+  border-radius: 5px;
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
+
+  &:not(.disable):hover {
+    background: #cad5f5;
+    -webkit-transform: translate3d(0, 0, 20px);
+    transform: translate3d(0, 0, 20px);
+    box-shadow: 30px 30px 10px rgba(0, 0, 0, 0.5);
   }
 }
-</script>
+
+.title-content {
+  // border: 1px solid red;
+  .num {
+    font-size: 4rem;
+    font-weight: 600;
+    color: rgb(255, 0, 0);
+  }
+  .text {
+    font-size: 2rem;
+    font-weight: 600;
+    color: white;
+  }
+}
+</style>
