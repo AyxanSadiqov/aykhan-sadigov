9ffdbfb450d719201c8f68075cb5e6c287d77c68
EDUSISCO\musta <ayxan.sadiq.98@gmail.com>
2023-05-27 20:10:59 +0300

    google analytics added. gtag

--- src/main.js
+++ src/main.js
@@ -6,7 +6,21 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
+import VueGtag from "vue-gtag";
 
 library.add(fas, fab);
 
-createApp(App).component('fa', FontAwesomeIcon).use(store).use(router).mount("#app");
+createApp(App)
+  .component("fa", FontAwesomeIcon)
+  .use(store)
+  .use(router)
+  .use(
+    VueGtag,
+    {
+      appName: "Portfolio",
+      pageTrackerScreenviewEnabled: true,
+      config: { id: "G-JZM6QV98BC" },
+    },
+    router
+  )
+  .mount("#app");
